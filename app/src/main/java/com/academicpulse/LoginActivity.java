a7634f8d6e173b0ad68754330dbbbbb3fbaec7da
MacBook Pro <fintech.ssource@gmail.com>
2026-06-06 10:10:11 +0700

    Update Message LoginActivity

--- app/src/main/java/com/academicpulse/LoginActivity.java
+++ app/src/main/java/com/academicpulse/LoginActivity.java
@@ -2,7 +2,6 @@ package com.academicpulse;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -47,9 +46,7 @@ public class LoginActivity extends AppCompatActivity {
             });
         });
 
-        findViewById(R.id.tv_register).setOnClickListener(v -> {
-            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-        });
+        findViewById(R.id.tv_register).setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
 
         findViewById(R.id.btn_back).setOnClickListener(v -> finish());
     }
